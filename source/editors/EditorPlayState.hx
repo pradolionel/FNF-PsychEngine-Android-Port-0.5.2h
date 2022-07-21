569b1ad1f7f3a97f817f19a905596d10256585f4
kamodrip <100513117+Akhia11@users.noreply.github.com>
2022-07-21 21:37:01 +0300

    Update EditorPlayState.hx

--- source/editors/EditorPlayState.hx
+++ source/editors/EditorPlayState.hx
@@ -150,6 +150,11 @@ class EditorPlayState extends MusicBeatState
 		tipText.borderSize = 2;
 		tipText.scrollFactor.set();
 		add(tipText);
+
+		#if android
+		addAndroidControls();
+		#end
+
 		FlxG.mouse.visible = false;
 
 		//sayGo();
@@ -835,13 +840,14 @@ class EditorPlayState extends MusicBeatState
 		var comboSpr:FlxSprite = new FlxSprite().loadGraphic(Paths.image(pixelShitPart1 + 'combo' + pixelShitPart2));
 		comboSpr.screenCenter();
 		comboSpr.x = coolText.x;
-		comboSpr.acceleration.y = FlxG.random.int(200, 300);
-		comboSpr.velocity.y -= FlxG.random.int(140, 160);
+		comboSpr.acceleration.y = 600;
+		comboSpr.velocity.y -= 150;
 		comboSpr.visible = !ClientPrefs.hideHud;
-		comboSpr.x += ClientPrefs.comboOffset[4];
-		comboSpr.y -= ClientPrefs.comboOffset[5];
+		comboSpr.x += ClientPrefs.comboOffset[0];
+		comboSpr.y -= ClientPrefs.comboOffset[1];
 
 		comboSpr.velocity.x += FlxG.random.int(1, 10);
+		comboGroup.add(rating);
 
 		if (!PlayState.isPixelStage)
 		{
@@ -869,7 +875,6 @@ class EditorPlayState extends MusicBeatState
 		seperatedScore.push(combo % 10);
 
 		var daLoop:Int = 0;
-		
 		for (i in seperatedScore)
 		{
 			var numScore:FlxSprite = new FlxSprite().loadGraphic(Paths.image(pixelShitPart1 + 'num' + Std.int(i) + pixelShitPart2));
@@ -896,14 +901,8 @@ class EditorPlayState extends MusicBeatState
 			numScore.velocity.x = FlxG.random.float(-5, 5);
 			numScore.visible = !ClientPrefs.hideHud;
 
-			if(combo >= 10)	
-			{				
-			insert(members.indexOf(strumLineNotes), comboSpr);
-			insert(members.indexOf(strumLineNotes), numScore);
-			}
-			
-			insert(members.indexOf(strumLineNotes), rating);
-
+			if (combo >= 10 || combo == 0)
+				insert(members.indexOf(strumLineNotes), numScore);
 
 			FlxTween.tween(numScore, {alpha: 0}, 0.2, {
 				onComplete: function(tween:FlxTween)
